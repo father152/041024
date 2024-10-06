4fa17083e134e4352bde7c483660ee543380491a
father152 <tsimbalenko.vv@gmail.com>
2024-10-06 06:19:01 +0300

    4554

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function App() {
+  const [likes, setLikes] = useState("0");
+  const [value, setValue] = useState("");
+  function increment() {
+    setLikes((likes += 1));
+  }
+  function decrement() {
+    setLikes((likes -= 1));
+  }
+
+  return (
+    <div className="app">
+      <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input
+        type="text"
+        value="{value} 
+    onChange={event => setValue(event.target.value)}" />
+     
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <div className="dfg"></div>
+    </div>
+  );
+}
+
+export default App;
